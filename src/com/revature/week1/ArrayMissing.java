c170b2c359f52655dd36b03cbdd607be85ade039
jkgmr13 <84328587+jkgmr13@users.noreply.github.com>
2021-08-02 12:57:36 -0600

    Update ArrayMissing.java

--- src/com/revature/week1/ArrayMissing.java
+++ src/com/revature/week1/ArrayMissing.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class ArrayMissing {
 
     public static List<Integer> missing(List<Integer> list){
+        Collections.sort(list);
         ArrayList<Integer> antiList = new ArrayList<>();
         for(int i = list.get(0); i < list.get(list.size()-1); i++){
             if(!list.contains(i)) antiList.add(i);
